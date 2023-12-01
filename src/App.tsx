52bffd01eaea32b5f98eb71f9e6782fc13e4f70e
Sebatora <sebastiana.toranzo@gmail.com>
2023-12-01 12:11:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const mockTodos = [
+  {
+    id: "1",
+    title: "todo 1",
+    completed: "false"
+  },
+  {
+    id: "2",
+    title: "todo 2",
+    completed: "false"
+  },
+  {
+    id: "3",
+    title: "todo 3",
+    completed: "false"
+  }
+]
+
+const App = (): JSX.Element => {
+  const [todos] = useState(mockTodos)
+
+  return (
+  <Todos todos={todos} />
+  )
+}
+
+export default App
